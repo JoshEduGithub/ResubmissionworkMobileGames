a592caf05300794d0a7b186dba3edf48469a437c
Josh <55580587+JoshEduGithub@users.noreply.github.com>
2022-07-16 13:21:25 +0100

    Improvements to game
    Added
    High score - saves to playerprefs
    Game assets - improved game assets character models and items
    Animation - food shrinks and expands in snake game
    Particle effect - game scene behind the arcade boxes

--- MobileGameDev-Re/MobileGame/Assets/Scripts/ThatWormGame/ScoreCounter.cs
+++ MobileGameDev-Re/MobileGame/Assets/Scripts/ThatWormGame/ScoreCounter.cs
@@ -6,18 +6,40 @@ using TMPro;
 public class ScoreCounter : MonoBehaviour
 {
     public TextMeshProUGUI tScore;
+    public TextMeshProUGUI hScore;
     private static float wormscore;
+    private float HighScore;
 
 
     void Start()
     {
        wormscore = 0;
+       // loads the players highest score when the scene opens
+       HighScore = PlayerPrefs.GetFloat("highScore", HighScore);
+       hScore.text = "Best: " + HighScore.ToString();
 
+    }
+
+    void Update()
+    {
+        if (wormscore > HighScore)
+        {   
+            // setting the high score to the players 
+            HighScore = wormscore;
+            // writing high score to gui
+            hScore.text = "Best: " +  HighScore;
+            // where the score will be saved
+            PlayerPrefs.SetFloat("highScore", HighScore);
+            // save the score to file
+            PlayerPrefs.Save();
+            
+        }
     }
     public void food(int _food)
     {
+        // increase score when get food
         wormscore += _food;
-
+        // writes score to gui
         tScore.text = "SCORE: " + wormscore;
     }
 
